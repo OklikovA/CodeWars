95a1dfba267fa35a93fb8c25148b78a280863568
OklikovA <motya.fzr600@gmail.com>
2026-09-08 17:23:01 +0300

    Добавлен get_middle

---
+++ get_middle.py
@@ -0,0 +1,12 @@
+def get_middle(s):
+    if len(s) % 2 != 0:
+        return s[len(s) // 2]
+    else:
+        return s[len(s) // 2 - 1:len(s) // 2 + 1]
+
+#s = "test"
+s = "testing"
+#s = "middle"
+#s = "A"
+
+print(get_middle(s))
